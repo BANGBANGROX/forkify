4e7af5b1acaaaf999a4201a280f214f3172edaee
BANGBANGROX <lakshya0809bang@gmail.com>
2021-10-04 19:48:44 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2bae3c12-3a91-4453-bd13-3bdced60ae90';
+export const MODAL_CLOSE_SEC = 2.5;
